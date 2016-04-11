ce26f926ad76ac7b11b28a809da07b6c30e172c8
Ephraim Becker <emb16@outlook.com>
2016-04-11 11:58:11 -0400

    Converted the Nav into LCARS

--- content scripts/contentScript.js
+++ content scripts/contentScript.js
@@ -4,6 +4,22 @@ var headerText = header.children;
 var nav = document.getElementsByTagName("nav")[0];
 var navText = nav.children;
 
+var navBlockOne = document.createElement("div");
+navBlockOne.setAttribute("class", "navBlock");
+navBlockOne.setAttribute("id", "navBlockOne");
+
+var navBlockTwo = document.createElement("div");
+navBlockTwo.setAttribute("class", "navBlock");
+navBlockTwo.setAttribute("id", "navBlockTwo");
+
+var navBlockThree = document.createElement("div");
+navBlockThree.setAttribute("class", "navBlock");
+navBlockThree.setAttribute("id", "navBlockThree");
+
+var navBlockFour = document.createElement("div");
+navBlockFour.setAttribute("class", "navBlock");
+navBlockFour.setAttribute("id", "navBlockFour");
+
 for(var i = 0; i < headerText.length; i++)
 {
     headerText[i].setAttribute("class", "headerText");
@@ -27,3 +43,13 @@ header.appendChild(topBlock);
 header.appendChild(topRightEndBlock);
 header.appendChild(topLeftCornerBlock);
 header.appendChild(topLeftCornerBlockTwo);
+
+nav.appendChild(navBlockOne);
+nav.appendChild(navBlockTwo);
+nav.appendChild(navBlockThree);
+nav.appendChild(navBlockFour);
+
+navBlockOne.appendChild(navText[0].children[0]);
+navBlockTwo.appendChild(navText[0].children[0]);
+navBlockThree.appendChild(navText[0].children[0]);
+navBlockFour.appendChild(navText[0].children[0]);
